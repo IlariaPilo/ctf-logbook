64f77d96a4a003a9017093e96a01cf5bd84ccc9f
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2024-08-12 16:20:34 +0200

    Change socket with pwn

--- utilities/connect.py
+++ utilities/connect.py
@@ -1,31 +1,30 @@
 # A script to connect to a server via TCP, and automatically reply with whatever logic you want!
-import socket
+from pwn import *
 
 def process(received_data):
     # Replace this function with the processing logic
     return ''
 
 def main():
-    server_address = ('94.237.59.199', 40534)
+    sock = remote('94.237.59.199', 40534)
+        
+    while True:
+        # Receive data from the server until `?`
+        received_data = sock.recvuntil(b"?", timeout=1).decode()
+        # or, get the line that contains `Message:`
+        # received_data = sock.recvline_contains(b"Message:", timeout=1).decode()
+        if not received_data:
+            print("No data received, exiting...")
+            break
+        print(f"{received_data}")
+        
+        # Process the received string
+        processed_data = process(received_data)
     
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect(server_address)
+        # Send the processed data back to the server
+        sock.sendline(processed_data.encode())
+        print(f"{processed_data}")
         
-        while True:
-            # Receive data from the server
-            received_data = sock.recv(1024).decode('utf-8')
-            if not received_data:
-                print("No data received, exiting...")
-                break
-            print(f"{received_data}")
-            
-            # Process the received string
-            processed_data = process(received_data)
-        
-            # Send the processed data back to the server
-            sock.sendall(processed_data.encode('utf-8'))
-            print(f"{processed_data}")
-            
 
 if __name__ == "__main__":
     main()
